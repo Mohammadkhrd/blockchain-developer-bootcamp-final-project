8815378d1c8a6c0877994a7e18fe24bbce997d95
Mohammadkhrd <you@example.commohammadkheradmandy@gmail.com>
2021-11-14 17:57:24 +0330

    connect wallet + account address + deposit eth

---
+++ client/dapp.js
@@ -0,0 +1,186 @@
+const LOTTERY_ADDRESS = '0x36e74C93620115e8f38fC78797Fb9f97463cA200'
+
+const LOTTERY_ABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "player",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Deposit",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "WithdrawMoney",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "reOpenLottery",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawMoney",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+const web3 = new Web3(window.ethereum)
+const Lottery = new web3.eth.Contract(LOTTERY_ABI, LOTTERY_ADDRESS)
+Lottery.setProvider(window.ethereum)
+
+window.addEventListener('load', async() => {
+
+  var balance = await Lottery.methods.getBalance().call()
+  var contractBalance = document.getElementById('contract-balance');
+  contractBalance.innerHTML = "Contract Balance : " + balance
+
+})
+
+const connectWallet = document.getElementById('connect-wallet');
+
+connectWallet.onclick = async () => {
+
+  await ethereum.request({ method: 'eth_requestAccounts'})
+  var walletAddress = document.getElementById('wallet-address');
+  walletAddress.innerHTML = ethereum.selectedAddress
+
+}
+
+const deposit = document.getElementById('deposit');
+
+deposit.onclick = async () => {
+  await Lottery.methods.deposit().send({from: ethereum.selectedAddress, value: 1000000000000000000})
+  
+}
+
+
+
+
+
